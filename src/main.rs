1181c7a0edcd570497a17433b62eadf525328cf8
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2024-03-03 12:34:12 +0530

    basic struct created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::net::IpAddr;
+
+struct Arguments{
+    flag: String,
+    ip_addr: IpAddr,
+    threads: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Variables should be in snake_case, not camel or pascal.
+    let user_args:Vec<String>=env::args().collect();
+
+    // Need to pass it as borrow otherwise it takes ownership implicitly.
+    for arg in &user_args{
+        println!("{:?}",arg);
+    }
+
+    
+
+    // To give arguments to the program and not to cargo, use cargo run -- <enter arguments here>
+    println!("{:?}",user_args);
 }
